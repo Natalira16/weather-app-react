925ab33b6b08380d0e6a18014f02cb85d4ec3e33
Natalira16 <medvednat@gmail.com>
2022-10-02 23:43:55 +0200

    Added axios integration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
+import Weather from './Weather';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          👋🏼 Hello World 👩🏽‍💻
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Weather city="Berlin" />
       </header>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,24 @@
+import React from "react";
+import axios from "axios";
+import { FallingLines } from 'react-loader-spinner'
+
+export default function Weather(props) {
+    function handleResponse(response) {
+        alert(`The weather in ${response.data.name} is ${Math.round(response.data.main.temp)}°C`)
+    }
+
+    let apiKey = "c9470fa529ce6770f386cc31e17d6a25";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return (
+        <div>
+            <h2>Hey from Weather</h2>
+            <FallingLines
+                color="#61dafb"
+                width="100"
+                visible={true}
+                ariaLabel='falling-lines-loading'
+            />
+        </div>
+    )
+}
